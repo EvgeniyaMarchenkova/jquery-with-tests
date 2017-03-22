76477c16cdd493476b9ea76304a5b08b5bfdab99
Evgeniya Marchenkova <evgeniova@gmail.com>
2017-03-22 17:08:45 +0300

    5 methods for .child is working

--- custom-jquery.js
+++ custom-jquery.js
@@ -1,18 +1,68 @@
 
 (window.$ = function(str) {
     var arrElm = document.querySelectorAll(str);
-    $.addClass = function (value) {
-        if (typeof value == 'string') {
-            arrElm.forEach(function(item) {
-                item.classList.add(...value.replace(/ +/, ' ').split(' '));
-            })
-        }
-        else if (typeof value == 'function') {
+    window.$.addClass = function (value) {
+        var index = 0;
+        [].forEach.call(arrElm, function(item) {
+            if (typeof value == 'string') {
+                addClassElm(item, value);
+            }
+            else if (typeof value == 'function') {
+                var result = value(index);
+                index++;
+                addClassElm(item, result);
+            }
+            function addClassElm(elm, elmClassName) {
+                elm.classList.add(...elmClassName.replace(/ +/, ' ').split(' '));
+            }
+        })
+    }
+    window.$.append = function (value) {
 
-        }
+        [].forEach.call(arrElm, function(item) {
+            if (typeof value == 'function') {
+                console.log(value.arguments)
+                var insert = document.querySelector(value.arguments[0]);
+                item.innerHTML += insert;
+                insert.remove();
+            }
+            else {
+                item.innerHTML += value;
+            }
+        })
 
     }
-    return $;
+    window.$.html = function(value) {
+        if (!value) {
+            return arrElm[0].innerHTML;
+        }
+        else {
+            [].forEach.call(arrElm, function(item) {
+               item.innerHTML = value;
+            })
+        }
+    }
+    window.$.attr = function(...value) {
+        if (value.length == 2) {
+            [].forEach.call(arrElm, function(item) {
+                item.setAttribute(value[0], value[1]);
+            })
+
+        }
+        return arrElm[0].getAttribute(value);
+    }
+    window.$.children = function(value) {
+        if (!value) {
+            return arrElm[0].children;
+        }
+        return [].filter.call(arrElm[0].children, function(item){
+            if (item.matches(value)) return true;
+            return false;
+        })
+
+    }
+
+    return window.$;
 
 })();
 

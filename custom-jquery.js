03b0cbdb85cbebf3ae5cf6551285ea53ef355550
Evgeniya Marchenkova <evgeniova@gmail.com>
2017-03-24 15:17:39 +0300

    jquery constructor added

--- custom-jquery.js
+++ custom-jquery.js
@@ -1,74 +1,87 @@
 
-(window.$ = function(str) {
-    var arrElm = document.querySelectorAll(str);
-    window.$.addClass = function (value) {
+window.$ = function (selector, context) {
+    function CustomJquery(selector) {
+        var arrElm = document.querySelectorAll(selector);
+        for (var i = 0; i < arrElm.length; i++) {
+            this[i] = arrElm[i];
+        }
+        this.selector = selector;
+    }
+    CustomJquery.prototype.addClass = function (value) {
         var index = 0;
-        [].forEach.call(arrElm, function(item) {
-            if (typeof value == 'string') {
-                addClassElm(item, value);
-            }
-            else if (typeof value == 'function') {
-                var result = value(index, item.className);
-                index++;
-                addClassElm(item, result);
-            }
-            function addClassElm(elm, elmClassName) {
-                elm.classList.add(...elmClassName.replace(/ +/, ' ').split(' '));
-            }
-        })
-    }
-    window.$.append = function (value) {
-        [].forEach.call(arrElm, function(item) {
-            if (typeof value == 'function') {
-                return function(selector) {
-                    console.log(selector);
-                    var node = document.querySelector(selector);
-                    item.appendChild(node);
+        for (node in this) {
+            if (/^[0-9]+$/.test(node)) {
+                if (typeof value == 'string') {
+                    addClassElm(this[node], value);
                 }
-
-
-
-                item.appendChild(node);
-                //node.remove();
+                else if (typeof value == 'function') {
+                    var result = value(index, this[node].className);
+                    index++;
+                    addClassElm(this[node], result);
+                }
+                function addClassElm(elm, elmClassName) {
+                    elm.classList.add(...elmClassName.replace(/ +/, ' ').split(' '));
+                }
             }
-            else {
-                item.innerHTML += value;
-            }
-        })
-
+        }
     }
-    window.$.html = function(value) {
+    CustomJquery.prototype.append = function (value) {
+        for (node in this) {
+            if (/^[0-9]+$/.test(node)) {
+                if (typeof value == 'function') {
+                    return function(selector) {
+                        var node = document.querySelector(selector);
+                        item.appendChild(node);
+                    }
+
+
+
+                    item.appendChild(node);
+                    //node.remove();
+                }
+                else {
+                    this[node].innerHTML += value;
+                }
+            }
+        }
+    }
+    CustomJquery.prototype.html = function(value) {
         if (!value) {
-            return arrElm[0].innerHTML;
+            return this[0].innerHTML;
         }
         else {
-            [].forEach.call(arrElm, function(item) {
-               item.innerHTML = value;
-            })
-        }
-    }
-    window.$.attr = function(...value) {
-        if (value.length == 2) {
-            [].forEach.call(arrElm, function(item) {
-                item.setAttribute(value[0], value[1]);
-            })
+            for (node in this) {
+                if (/^[0-9]+$/.test(node)) {
+                    this[node].innerHTML = value;
 
+                }
+            }
         }
-        return arrElm[0].getAttribute(value);
     }
-    window.$.children = function(value) {
-        if (!value) {
-            return arrElm[0].children;
+    CustomJquery.prototype.attr = function(...value) {
+        if (value.length == 2) {
+            for (node in this) {
+                if (/^[0-9]+$/.test(node)) {
+                    this[node].setAttribute(value[0], value[1]);
+
+                }
+            }
         }
-        return [].filter.call(arrElm[0].children, function(item){
+        return this[0].getAttribute(value);
+    }
+    CustomJquery.prototype.children = function(value) {
+        if (!value) {
+            return this[0].children;
+        }
+        return [].filter.call(this[node].children, function(item){
             if (item.matches(value)) return true;
             return false;
         })
 
     }
-    window.$.css = function(value) {
+    CustomJquery.prototype.css = function(value) {
         if (typeof value == 'string') {
-            return arrElm[0].style.value;
+            return this[0].style.value;
         }
         else {
             value.toString = function() {
@@ -78,31 +91,96 @@
                 }
                 return str;
             }
-            arrElm[0].style.cssText = value.toString();
+            this[0].style.cssText = value.toString();
         }
     }
-    window.$.on = function(evnt, func) {
-        [].forEach.call(arrElm, function(item) {
-            item.setAttribute('on' + evnt, func);
-        })
-    }
-    window.$.on = function(evnt, func) {
-        [].forEach.call(arrElm, function(item) {
-            var isCalled = false;
-            if (isCalled) {
-                isCalled = true;
-                item.setAttribute('on' + evnt, func);
-            }
-        })
-    }
+    CustomJquery.prototype.on = function(evnt, func) {
+        for (node in this) {
+            if (/^[0-9]+$/.test(node)) {
+                this[node].setAttribute('on' + evnt, func);
 
-    window.$.each = function(func) {
-        [].forEach.call(arrElm, function(item) {
+            }
+        }
+    }
+    CustomJquery.prototype.one = function(evnt, func) {
+        for (node in this) {
+            if (/^[0-9]+$/.test(node)) {
+                let isCalled = false;
+                if (isCalled) {
+                    isCalled = true;
+                    this[node].setAttribute('on' + evnt, func);
+                }
+
+            }
+        }
+
+    }
+    CustomJquery.prototype.each = function (func) {
+        [].forEach.call(this, function(item) {
             func.apply(item, func.arguments);
         })
     }
-    return window.$;
-})();
+
+
+
+
+
+    return new CustomJquery(selector);
+};
+/*
+
+
+
+
+
+
+
+ window.$.children = function(value) {
+ if (!value) {
+ return arrElm[0].children;
+ }
+ return [].filter.call(arrElm[0].children, function(item){
+ if (item.matches(value)) return true;
+ return false;
+ })
+
+ }
+ window.$.css = function(value) {
+ if (typeof value == 'string') {
+ return arrElm[0].style.value;
+ }
+ else {
+ value.toString = function() {
+ var str = '';
+ for (key in value) {
+ str += key + ':' + value.key + '; ';
+ }
+ return str;
+ }
+ arrElm[0].style.cssText = value.toString();
+ }
+ }
+ window.$.on = function(evnt, func) {
+ [].forEach.call(arrElm, function(item) {
+ item.setAttribute('on' + evnt, func);
+ })
+ }
+ window.$.on = function(evnt, func) {
+ [].forEach.call(arrElm, function(item) {
+ var isCalled = false;
+ if (isCalled) {
+ isCalled = true;
+ item.setAttribute('on' + evnt, func);
+ }
+ })
+ }
+
+ window.$.each = function(func) {
+ [].forEach.call(arrElm, function(item) {
+ func.apply(item, func.arguments);
+ })
+ }
+ */
 
 
 

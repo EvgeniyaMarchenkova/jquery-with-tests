0b63ff47b0d4075548070eb149ac1e22ef31cfc4
Evgeniya Marchenkova <evgeniova@gmail.com>
2017-03-24 13:04:43 +0300

    all methods updated

--- custom-jquery.js
+++ custom-jquery.js
@@ -8,7 +8,7 @@
                 addClassElm(item, value);
             }
             else if (typeof value == 'function') {
-                var result = value(index);
+                var result = value(index, item.className);
                 index++;
                 addClassElm(item, result);
             }
@@ -18,13 +18,18 @@
         })
     }
     window.$.append = function (value) {
-
         [].forEach.call(arrElm, function(item) {
             if (typeof value == 'function') {
-                console.log(value.arguments)
-                var insert = document.querySelector(value.arguments[0]);
-                item.innerHTML += insert;
-                insert.remove();
+                return function(selector) {
+                    console.log(selector);
+                    var node = document.querySelector(selector);
+                    item.appendChild(node);
+                }
+
+
+
+                item.appendChild(node);
+                //node.remove();
             }
             else {
                 item.innerHTML += value;
@@ -61,9 +66,42 @@
         })
 
     }
+    window.$.css = function(value) {
+        if (typeof value == 'string') {
+            return arrElm[0].style.value;
+        }
+        else {
+            value.toString = function() {
+                var str = '';
+                for (key in value) {
+                    str += key + ':' + value.key + '; ';
+                }
+                return str;
+            }
+            arrElm[0].style.cssText = value.toString();
+        }
+    }
+    window.$.on = function(evnt, func) {
+        [].forEach.call(arrElm, function(item) {
+            item.setAttribute('on' + evnt, func);
+        })
+    }
+    window.$.on = function(evnt, func) {
+        [].forEach.call(arrElm, function(item) {
+            var isCalled = false;
+            if (isCalled) {
+                isCalled = true;
+                item.setAttribute('on' + evnt, func);
+            }
+        })
+    }
 
+    window.$.each = function(func) {
+        [].forEach.call(arrElm, function(item) {
+            func.apply(item, func.arguments);
+        })
+    }
     return window.$;
-
 })();
 
 
